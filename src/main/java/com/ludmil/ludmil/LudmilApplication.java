1fe4f64efc966761903f2b39a4f83af88d95f571
ludmil7703 <106627152+ludmil7703@users.noreply.github.com>
2023-09-29 22:01:51 +0300

    First commit with IntelliJ.

---
+++ src/main/java/com/ludmil/ludmil/LudmilApplication.java
@@ -0,0 +1,13 @@
+package com.ludmil.ludmil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudmilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudmilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ludmil/ludmil/LudmilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ludmil.ludmil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudmilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
